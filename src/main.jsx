d4048e168ac96652bc72856133a25d3716c7208f
Dein Namealexisseshier <alex.dri@gmx.de>
2025-06-18 19:46:55 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'  // importiere die App-Komponente
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
